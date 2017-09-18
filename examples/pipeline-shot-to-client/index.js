0afd8806e50cf478034eaf1c4f3b5d8c10bb9604
lolwho <lolwhoami@users.noreply.github.com>
2017-09-18 20:18:25 +0300

    added comments

--- examples/pipeline-shot-to-client/index.js
+++ examples/pipeline-shot-to-client/index.js
@@ -5,39 +5,59 @@ const cmd = require('commander'),
 	shotty = require('shotty-api')(...require('./shotty.json')),
 	ftp = require('ftp');
 
+/*
+	setting up commander to read command line arguments
+*/
 cmd.version('6.6.6')
 	.option('-p, --project <project>', 'Project code')
 	.option('-u, --users [<email | id>, ...]', 'User email address or ID, one or multiple splitted by commas')
 	.parse(process.argv);
 
+/*
+	project code is required
+*/
 if(!cmd.project)
 	process.exit(console.error('Please, specify a project code!'), cmd.outputHelp());
 
+/*
+	setting up ftp uploader
+*/
 const upload = file => new Promise((resolve, reject) => {
 	let c = new ftp();
-	c.on('ready', () =>
-		c.put(file, '/ftp/' + require('path').parse(file).base, err => err ? reject(err) : resolve(c.end()))
+
+	c.on('ready', () => // setting up callback function which fires when ftp-connection is ready
+		c.put(
+			file, // path to a file to upload
+			'/ftp/' + require('path').parse(file).base, // where to store it on ftp server
+			err => err ? reject(err) : resolve(c.end()) // callback: checking if there was an error and resolving the promise if not
+		)
 	);
-	c.connect(require('./ftp.json'));
+
+	c.connect(require('./ftp.json')); // starting ftp-connection
 });
 
-const notifyUser = async shot => {
-	let allUsers = await shotty.get.users(),
-		selectedUsers = cmd.users.split(',').map(u => u.trim()),
-		users = allUsers.filter(au => au.role === 'superuser' || selectedUsers.findIndex(su => au.id === su || au.local.email === su));
+/*
+	setting up notify function
+*/
+const notifyUsers = async shot => {
+	let allUsers = await shotty.get.users(), // getting all users
+		selectedUsers = cmd.users ? cmd.users.split(',').map(u => u.trim()) : [], // splitting -u argument value into array of ids and emails
+		users = allUsers.filter(au => // getting superusers and users specified in -u argument
+			au.role === 'superuser' || selectedUsers.findIndex(su => au.id === su || au.local.email === su)
+		);
 
 	if(!users.length)
 		throw Error('No users found!');
 
-	let versions = await shotty.get.versions(cmd.project, shot.id),
-		latestVersion = versions.sort((a, b) => new Date(b.date) - new Date(a.date))[0];
+	let versions = await shotty.get.versions(cmd.project, shot.id), // getting versions corresponding to project code and shot
+		latestVersion = versions.sort((a, b) => new Date(b.date) - new Date(a.date))[0]; // selecting the latest version
 
 	try {
-		await upload('/media/shotty/storage/' + latestVersion.file.url);
+		await upload('/media/shotty/storage/' + latestVersion.file.url); // uploading a file to the ftp server
 
 		// Notification scheme: https://github.com/sick/shotty-api/blob/master/js/schemes.md#notifications
 		return await new Promise((resolve, reject) =>
-			Promise.all(
+			Promise.all( // creating notifications for all users and waiting for them all to be created
 				users.map(user =>
 					shotty.create('notification', {
 						userId: user.id,
@@ -83,14 +103,19 @@ const notifyUser = async shot => {
 	}
 };
 
+/*
+	finally, launching the watcher
+*/
 shotty.connect()
 .then(() => {
 	shotty.changes('shots', {onConnect: () => console.log('Watching for changes in shots...')})
 	.onDisconnect(() => console.log('Stopped watching shots.'))
-	.onUpdate((shot, oldShot) => shot.projectId !== cmd.project || shot.status !== 'client' || oldShot.status === 'client' ? false :
-		notifyUser(shot)
-		.then(() => console.log('Notified user about shot', shot.sequence, shot.code))
-		.catch(error => console.error(`Couldn't notify user about changes in shot <${shot.sequence} ${shot.code}>:`, error))
+	.onUpdate((shot, oldShot) =>
+		shot.projectId !== cmd.project || shot.status !== 'client' || oldShot.status === 'client'
+		? false // if the updated shot is not in the specified project, or status is not <client>, or old status is <client> — skip the event
+		: notifyUsers(shot) // otherwise — notify users
+		.then(() => console.log('Notified users about shot', shot.sequence, shot.code))
+		.catch(error => console.error(`Couldn't notify users about changes in shot <${shot.sequence} ${shot.code}>:`, error))
 	);
 })
-.catch(console.error);
+.catch(error => console.error('Could not connect to SHOTTY', error));
